e845324695562d5b21d92eaa6bae082e7db30980
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2023-12-11 22:20:19 -0300

    implements model loading

--- classificator/Classificator.py
+++ classificator/Classificator.py
@@ -1,5 +1,8 @@
 class Classificator:
 
+    def load(self, model_file: str):
+        raise NotImplementedError()
+
     def train(self, X_train, Y_train):
         raise NotImplementedError()
     
--- classificator/SVMClassificator.py
+++ classificator/SVMClassificator.py
@@ -1,10 +1,16 @@
-from sklearn.svm import SVC
+import pickle
 from Classificator import Classificator
 
-class SVMClassificator(Classificator, SVC):
+class SVMClassificator(Classificator):
+    __model = None
 
     def __init__(self):
         super().__init__()
+
+    def load(self, model_file: str):
+        pickle_input = open(model_file, 'rb')
+        self.__model = pickle.load(pickle_input)
+        pickle_input.close()
 
     def train(self, X_train, Y_train):
         self.fit(X_train, Y_train)
